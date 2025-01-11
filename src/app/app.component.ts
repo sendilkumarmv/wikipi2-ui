e7a87f7eca54f7971ad30dd9cdd30be7ac15c2e3
Sendilkumar Murugaiah <sendilkumarmurugaiah@gmail.com>
2025-01-11 21:09:07 +0530

    breadcrumbs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -50,21 +50,23 @@ export class AppComponent implements OnInit {
     this.router.navigate(['/create-account']);
   }
 
-  private createBreadcrumbs(route: ActivatedRoute, url: string = '', 
-        breadcrumbs: Array<{ label: string, url: string }> = []): Array<{ label: string, url: string }> {
-    const children: ActivatedRoute[] = route.children; 
-    if (children.length === 0) { 
-      return breadcrumbs; 
-    } 
-    for (const child of children) { 
-      const routeURL: string = child.snapshot.url.map(segment => segment.path).join('/'); 
-      if (routeURL !== '') { 
-        url += `/${routeURL}`; 
-      } 
-      const label = child.snapshot.data['breadCrumb']; 
-      if (label) { 
-        breadcrumbs.push({ label, url }); 
-      } 
-      return this.createBreadcrumbs(child, url, breadcrumbs); } return breadcrumbs;
+  private createBreadcrumbs(route: ActivatedRoute, url: string = '',
+    breadcrumbs: Array<{ label: string, url: string }> = []): Array<{ label: string, url: string }> {
+    const children: ActivatedRoute[] = route.children;
+    if (children.length === 0) {
+      return breadcrumbs;
+    }
+    for (const child of children) {
+      const routeURL: string = child.snapshot.url.map(segment => segment.path).join('/');
+      if (routeURL !== '') {
+        url += `/${routeURL}`;
+      }
+      const label = child.snapshot.data['breadCrumb'];
+      if (label) {
+        breadcrumbs.push({ label, url });
+      }
+      return this.createBreadcrumbs(child, url, breadcrumbs);
+    }
+    return breadcrumbs;
   }
 }
